b63bb1070756c9043852c4b7e0a14a2406af9bd8
Hector <hector.pasantes.7e9@itb.cat>
2025-11-06 12:40:25 +0100

    ej2

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿public class ejercicio2  {
+    static void Main()
+    {
+        const string Menu = "Introdueix un número (0 per a sortir)";
+        const string Divisio = " Els numeros divisors de ";
+        int op, num, divisor, result, enter = 0;
+
+
+        do
+        {
+            Console.WriteLine(Menu);
+            op = Convert.ToInt32(Console.ReadLine());
+            num = op;
+            divisor = num;
+            while (divisor > 0)
+            {
+                result = num % divisor;
+                if (result == 0)
+                {
+                    enter = divisor;
+                    divisor--;
+                }else
+                {
+                    divisor--;
+                }
+
+            }
+            Console.WriteLine(Divisio + num + " són: " + enter);
+        } while (op != 0);
+    }
+}
