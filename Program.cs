347c6538c1a2dddcd10893f4d4875d3323a738b5
Hector <hector.pasantes.7e9@itb.cat>
2025-11-06 12:17:07 +0100

    ej1

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+public class exercici2
+{
+    static void Main()
+
+    {
+        const string Lost = "You've lost";
+        const string Winner = "You've won";
+        const string Traps = "trap found";
+        const string Coin = "You found a coin";
+        const string TotalCoin = "You got: ";
+
+        int coins = 0, lives = 3, trap = 0;
+        Random rnd = new Random();
+
+
+        for (lives = 3; lives > 0;)
+        {
+            trap = rnd.Next(1, 2);
+            if (trap == 1)
+            {
+                lives--;
+                Console.WriteLine(Traps);
+            }
+            else
+            {
+                if (coins < 5)
+                {
+                    coins++;
+                    Console.WriteLine(Coin);
+                    Console.WriteLine(TotalCoin + coins);
+                }else
+                {
+                    lives = 0;
+                }
+            }
+        }
+        
+        if (coins >= 5)
+        {
+            Console.WriteLine(Winner);
+        }
+        else
+        {
+            Console.WriteLine(Lost);
+        }
+    }
+}
